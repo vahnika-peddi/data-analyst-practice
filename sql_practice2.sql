17ae947ed61707d3383824c54332594dcf2930af
Vahnika Peddi <vahnikap@gmail.com>
2026-03-16 19:25:25 +0530

    added sql students practice2

---
+++ sql_practice2.sql
@@ -0,0 +1,22 @@
+-- sql practice
+
+CREATE TABLE students (
+  id INTEGER,
+  name TEXT,
+  course TEXT,
+  marks INTEGER
+);
+
+
+INSERT INTO students VALUES (1,'Ravi','Python',85);
+INSERT INTO students VALUES (2,'Anu','SQL',78);
+INSERT INTO students VALUES (3,'Kiran','Python',90);
+INSERT INTO students VALUES (4,'Meena','Excel',70);
+INSERT INTO students VALUES (5,'Ajay','SQL',88);
+
+
+SELECT * FROM students;
+
+SELECT name FROM students;
+
+SELECT marks FROM students;
