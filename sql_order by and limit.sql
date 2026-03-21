93fb4f15474a943db5583b412b41b56fc83859b3
Vahnika Peddi <vahnikap@gmail.com>
2026-03-21 18:38:39 +0530

    added SQL order by and limit

---
+++ sql_order by and limit.sql
@@ -0,0 +1,23 @@
+CREATE TABLE students (
+  id INTEGER,
+  name TEXT,
+  course TEXT,
+  marks INTEGER
+);
+
+INSERT INTO students VALUES (1,'Ravi','Python',85);
+INSERT INTO students VALUES (2,'Anu','SQL',78);
+INSERT INTO students VALUES (3,'Kiran','Python',90);
+INSERT INTO students VALUES (4,'Meena','Excel',70);
+INSERT INTO students VALUES (5,'Ajay','SQL',88);
+
+SELECT * FROM students
+ORDER BY marks DESC;
+
+SELECT * FROM students
+ORDER BY marks DESC
+LIMIT 2;
+
+SELECT * FROM students
+ORDER BY marks
+LIMIT 1;
