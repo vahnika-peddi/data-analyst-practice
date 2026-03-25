0198fdc212ab1f4ff8b58e2d2e0c5ee930a2b066
Vahnika Peddi <vahnikap@gmail.com>
2026-03-25 19:17:02 +0530

    added sql case when practice

---
+++ sql_case_when.sql
@@ -0,0 +1,20 @@
+CREATE TABLE students (
+id INTEGER,
+name TEXT,
+course TEXT,
+marks INTEGER
+);
+
+INSERT INTO students VALUES (1,'Ravi','Python',85);
+INSERT INTO students VALUES (2,'Anu','SQL',78);
+INSERT INTO students VALUES (3,'Kiran','Python',90);
+INSERT INTO students VALUES (4,'Meena','Excel',70);
+INSERT INTO students VALUES (5,'Ajay','SQL',88);
+
+SELECT name, marks,
+CASE
+WHEN marks >= 85 THEN 'High'
+WHEN marks >= 75 THEN 'Medium'
+ELSE 'Low'
+END AS performance
+FROM students;
